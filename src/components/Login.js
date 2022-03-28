e4a66574a80a8543ee91df98137c91b87571fd97
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-28 16:20:22 -0700

    Loading displays & init form with styling done. Form doesn't hold values yet

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,8 +8,8 @@ function Login() {
   let handleLoading = (loadTime) => {
     const timer = setInterval(() => {
       loadTime--;
-      loadTime % 2 === 0 && loadTime < 5
-        ? setTimerText(null)
+      loadTime % 2 === 0 && loadTime < 7
+        ? setTimerText("   ")
         : setTimerText("Loading...");
 
       if (loadTime <= 0) {
@@ -17,16 +17,41 @@ function Login() {
         setTimerText(null);
       }
       //console.log(loadTime);
-    }, 500);
+    }, 600);
   };
   //useEffect allows this fxn to be called only once and right when the page loads
   useEffect(() => {
     handleLoading(7);
   }, []);
 
+  function Form() {
+    //console.log("HERE");
+    const [isBlinking, setIsBlinking] = useState(true);
+
+    function handleFormInputValue(e) {
+      console.log(e.target.value);
+      if (e.target.value) setIsBlinking(false);
+    }
+
+    return (
+      <form>
+        <label id="form-label">Coder Name</label>
+        <input
+          type="text"
+          id={isBlinking ? "form-input" : "form-input-before"}
+          style={{ border: "none", autoFocus: true }}
+          placeholder="_"
+          onChange={handleFormInputValue}
+        />
+        <input type="submit" id="form-submit" />
+      </form>
+    );
+  }
+
   return (
     <div className="login-component">
       <div id="timer-text">{timerText}</div>
+      <div className="form-wrapper">{!timerText ? <Form /> : null}</div>
     </div>
   );
 }
