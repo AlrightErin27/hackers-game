75ff3bb565577049aa7919219e7a3d0d83a6f37e
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-29 16:44:36 -0700

    made images of game questions for formatting that are imported in Question.js also changed css in game screen

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import img01 from "../questions/q01.jpg";
+import img02 from "../questions/q02.jpg";
+import img03 from "../questions/q03.jpg";
+import img04 from "../questions/q04.jpg";
+import img05 from "../questions/q05.jpg";
 
 function Question({ question, nextQuestion }) {
+  const [currImg, setCurrImg] = useState("");
+
   // console.log(question[0].answers)
   const { id, questionPrompt, answers, correctAnswer } = question[0];
-  //console.log(answers)
+
+  //changes image when ever button is clicked
+  //image position in arr matches question id
+  const imgArr = [img01, img02, img03, img04, img05];
+  useEffect(() => {
+    for (let i = 1; i < imgArr.length; i++) {
+      if (id === i) {
+        setCurrImg(imgArr[i].toString());
+      }
+    }
+  }, [nextQuestion]);
+
   return (
     <>
       <h3>Question {id}</h3>
-      <h4 id="question-prompt">{questionPrompt}</h4>
+      <img src={currImg} alt={questionPrompt} className="q-images" />
       {answers.map((answer, index) => {
         const isCorrect = index === correctAnswer;
         let answerLetter = "A";
@@ -19,7 +37,7 @@ function Question({ question, nextQuestion }) {
           answerLetter = "D";
         }
         return (
-          <div key={answerLetter}>
+          <div key={answerLetter} className="answer-container">
             <button
               className="answer-button"
               key={answer}
